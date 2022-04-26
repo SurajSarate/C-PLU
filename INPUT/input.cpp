d796044ab5ed2e03d5b3f02044c69d0ac1f3f3dd
Suraj Sarate <88180480+SurajSarate@users.noreply.github.com>
2022-04-26 19:45:03 +0530

    input

---
+++ INPUT/input.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main (){
+    int amount1,amount2;
+    cout<<"Enter First Amount"<<endl;
+    cin >> amount1;
+
+    cout<<"Enter Second Amount"<<endl;
+    cin >> amount2;
+
+    int sum=amount1+amount2;
+    cout <<"Sum of  both amounts: " << sum << endl;
+
+    return 0;
+}
